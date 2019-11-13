08d5ce1293ebea3584135c100e1cccf8a83f4dbe
Terry Wang <tywang@uchicago.edu>
2019-11-13 10:21:47 -0600

    Add scripts for generating training samples

---
+++ generate_training_samples.py
@@ -0,0 +1,77 @@
+import glob
+import datetime
+import time
+import sys
+import os
+import random
+
+import numpy as np
+
+from pretty_midi_musGen.musGen_utils import generate_samples
+
+
+def main(source_folder, samples_folder, sample_length, n_samples):
+    """
+    generate samples from folder containing .npy files, with specified length and num of samples
+    :param source_folder: folder containing .npy files
+    :param samples_folder: target folder to write to
+    :param sample_length: how many notes should each sample contain
+    :param n_samples: how many samples should we generate
+    :return: nothing - samples written as .npy file to samples folder with a .txt log
+
+    ## Note: .npy files must exist directly under source folder and not under sub folders
+    """
+
+    output = []
+
+    # create directory if not exist
+    if not os.path.exists(samples_folder):
+        os.makedirs(samples_folder)
+
+    # iterable of all .npy files
+    all_files = glob.glob(source_folder + '/*.npy')
+    num_files = len(all_files)
+    # print(num_files)
+
+    # record which files were processed/unprocessed
+    processed_files = []
+    unprocessed_files = []
+
+    num_sampled = 0
+    while num_sampled < n_samples:
+        # randomly pick one file
+        sample_file = all_files[random.randint(0, num_files-1)]
+        # get 1 random sample from that file
+        try:
+            samp = generate_samples(sample_file, sample_length, 1)
+            processed_files.append(sample_file)
+            # print("successfully processed " + sample_file)
+            output.append(samp)
+            num_sampled += 1
+        except:
+            print("Index error, no sample generated")
+            unprocessed_files.append(sample_file)
+
+    timestamp = time.time()
+    st = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d-%H-%M')
+    np.save(samples_folder+'/'+'sample_'+st+'.npy', np.array(output).reshape((n_samples, sample_length, 3)))
+
+    processed_files = list(set(processed_files))
+
+    print("sampled from %d files" % len(processed_files))
+    print("generated %d samples" % num_sampled)
+
+    with open(samples_folder+'/'+'run_log_'+st+'.txt', 'w') as f:
+        for pf in processed_files:
+            f.write(pf+'\n')
+
+    with open(samples_folder+'/'+'run_log_unprocessed'+st+'.txt', 'w') as f:
+        for pf in processed_files:
+            f.write(pf+'\n')
+
+
+if __name__ == '__main__':
+    sys.exit(main(source_folder=sys.argv[1],
+                  samples_folder=sys.argv[2],
+                  sample_length=int(sys.argv[3]),
+                  n_samples=int(sys.argv[4])))
---
+++ write_midi_as_np.py
@@ -0,0 +1,78 @@
+import glob
+import datetime
+import time
+import sys
+import os
+
+import numpy as np
+
+from pretty_midi_musGen.musGen_utils import getDataFromMidi
+
+
+def main(source_filepath, target_filepath, target_key='None', max_n_files='None'):
+    """
+    Write all midi files into numpy array with key and tempo adjustment (in future release)
+    :param source_filepath: folder containing midi files
+    :param target_filepath: folder to write numpy arrays to
+    :param target_key: target key to convert to, pass string 'None' if no conversion
+    :param max_n_files: max number of files to be processed to avoid consuming too much space, pass 'None' for no limit
+    :return: nothing - numpy files written to disk in loop to save memory
+    in addition, write a .txt file with all processed files
+    """
+
+    # Check target key to make it correct
+    if target_key == 'None':
+        tkey = None
+    else:
+        tkey = int(target_key)
+
+    # create directory if not exist
+    if not os.path.exists(target_filepath):
+        os.makedirs(target_filepath)
+
+    # record which files were processed
+    processed_files = []
+
+    if max_n_files == 'None':
+        for file in glob.iglob(source_filepath+'/**/*.mid'):
+            in_fname = str(file)
+            # print('processing ' + in_fname)
+            out_fname = in_fname.split('/')[-1]
+            try:
+                np_midi = getDataFromMidi(file, tkey)
+                np.save(target_filepath+'/'+out_fname+'.npy', np_midi)
+                processed_files.append(in_fname)
+                # print('processed ' + in_fname)
+            except:
+                print(in_fname+" unable to be processed, continuing")
+                continue
+
+    else:
+        nfiles_processed = 0
+        for file in glob.iglob(source_filepath+'/**/*.mid'):
+            if nfiles_processed > int(max_n_files):
+                break
+            in_fname = str(file)
+            out_fname = in_fname.split('/')[-1]
+            try:
+                np_midi = getDataFromMidi(file, tkey)
+                np.save(target_filepath+'/'+out_fname+'.npy', np_midi)
+                processed_files.append(in_fname)
+                # print('processed ' + in_fname)
+            except:
+                print(in_fname+" unable to be processed, continuing")
+                continue
+
+    # write txt file for processed files
+    timestamp = time.time()
+    st = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d-%H-%M')
+    with open(target_filepath+'/'+'processed'+'_'+st+'.txt', 'w') as f:
+        for pf in processed_files:
+            f.write(pf+'\n')
+
+
+if __name__ == '__main__':
+    sys.exit(main(source_filepath=sys.argv[1],
+                  target_filepath=sys.argv[2],
+                  target_key=sys.argv[3],
+                  max_n_files=sys.argv[4]))
